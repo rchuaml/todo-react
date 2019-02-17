82ba1aa34e1f97274410d80e892840c007a74a5d
rchuaml <ronchuaml@gmail.com>
2019-02-17 23:16:48 +0800

    added error message and validations for input

--- script.js
+++ script.js
@@ -7,17 +7,28 @@ class List extends React.Component {
 
   state = {
     list : [],
-    word : ""
+    word : "",
+    error: ""
   }
 
   changeHandler(event){
     this.setState({word:event.target.value});
+    this.setState({error:""});
     console.log("change", event.target.value);
   }
 
   clickHandler(){
+
+    if(this.state.word.length > 1 && this.state.word.length <20){
     this.state.list.push(this.state.word);
     this.setState({ word : "" });
+    }else{
+        if(this.state.word.length<=1){
+            this.setState({error: "Error, input must be more than 1 characters in length"});
+        }else if (this.state.word.length>=20){
+            this.setState({error: "Error, input must be less than 20 characters in length"});
+        }
+    }
   }
 
   render() {
@@ -25,12 +36,15 @@ class List extends React.Component {
 
       console.log("rendering");
       return (
+        <div>
+        <div className = "error">{this.state.error}</div>
         <div className="list">
           <input onChange={this.changeHandler} value={this.state.word}/>
           <button onClick = {this.clickHandler}>add item</button>
           <ul>{this.state.list.map(x=>{
             return <li>{x}</li>;
           })}</ul>
+        </div>
         </div>
       );
   }
