0089fc9967e9ee7bcb77216aff2c4637f308a39b
rchuaml <ronchuaml@gmail.com>
2019-02-18 00:21:11 +0800

    addded delete function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ class List extends React.Component {
     super()
     this.changeHandler = this.changeHandler.bind( this );
     this.clickHandler = this.clickHandler.bind( this );
+    // this.deleteHandler = this.deleteHandler.bind( this );
   }
 
   state = {
@@ -12,23 +13,34 @@ class List extends React.Component {
   }
 
   changeHandler(event){
+    //react waits for both setstates to happen before rendering(proven by console log)
+    console.log("before setstate of event.target.value")
     this.setState({word:event.target.value});
+    console.log("before setstate of error")
     this.setState({error:""});
-    console.log("change", event.target.value);
-  }
+}
+
 
   clickHandler(){
-
     if(this.state.word.length > 1 && this.state.word.length <20){
-    this.state.list.push(this.state.word);
-    this.setState({ word : "" });
-    }else{
-        if(this.state.word.length<=1){
-            this.setState({error: "Error, input must be more than 1 characters in length"});
-        }else if (this.state.word.length>=20){
-            this.setState({error: "Error, input must be less than 20 characters in length"});
-        }
+        var fakeArr = this.state.list;
+        fakeArr.push(this.state.word);
+        console.log("this.state.list", this.state.list);
+        this.setState({ list: fakeArr});
     }
+    else if(this.state.word.length<=1){
+            this.setState({error: "Error, input must be more than 1 characters in length"});
+    }
+    else if (this.state.word.length>=20){
+            this.setState({error: "Error, input must be less than 20 characters in length"});
+    }
+    }
+
+
+  deleteHandler(index){
+    var listArray = this.state.list;
+    listArray.splice(index,1);
+    this.setState({list:listArray});
   }
 
   render() {
@@ -37,14 +49,13 @@ class List extends React.Component {
       console.log("rendering");
       return (
         <div>
-        <div className = "error">{this.state.error}</div>
-        <div className="list">
-          <input onChange={this.changeHandler} value={this.state.word}/>
-          <button onClick = {this.clickHandler}>add item</button>
-          <ul>{this.state.list.map(x=>{
-            return <li>{x}</li>;
-          })}</ul>
-        </div>
+            <div className = "error">{this.state.error}</div>
+            <input onChange={this.changeHandler} value={this.state.word}/>
+            <button onClick = {this.clickHandler}>add item</button>
+            <ul>{this.state.list.map((listitem , index)=>{
+                return <li>{listitem}<button onClick = {() => {this.deleteHandler(index)}}> Delete this item</button></li>;
+                })}
+            </ul>
         </div>
       );
   }
